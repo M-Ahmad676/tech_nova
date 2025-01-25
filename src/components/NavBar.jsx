57f16e6595fdcb1335bfbf4ded1f210ee41c470d
M-Ahmad676 <125358039+M-Ahmad676@users.noreply.github.com>
2025-01-25 19:55:26 +0500

    update1.5.1

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -15,9 +15,9 @@ export default function NavBar() {
   const [isScrolled, setIsScrolled] = useState(false)
   const [sideMenu ,setSideMenu] = useState(false)
   const navigate = useNavigate()
-
   const Location = useLocation()
   
+
   const handleSideMenu = () => {
        
     setSideMenu(!sideMenu)
@@ -79,11 +79,11 @@ export default function NavBar() {
     <div className={`flex justify-between px-4 sm:px-16 xl:px-0 xl:justify-around  items-center gap-x-10 h-16 bg-black fixed w-full z-10 transition-all duration-300 ${isScrolled ? 'top-0' : 'top-0 lg:top-[3.5rem]'}`}>
 
       <IoMenu className='text-white text-[1.7rem] sm:text-[2rem] block xl:hidden cursor-pointer hover:text-blue-400' onClick={handleSideMenu}/> 
-
+    {/* Side Bar */}
       <div className={`absolute bg-[#171717] min-h-screen w-[15rem] md:w-[19rem] h-full top-16 left-0 transition-all duration-300 ${sideMenu ? 'translate-x-0' : '-translate-x-full'}`}>
       <ul className='py-10 text-white text-[0.9rem] sm:text-[1.1rem]'>
         {Links.map((link,index) => (
-        <Link to={link.Path} key={index} className={`px-4 py-5 sm:px-8 flex items-center gap-x-5 hover:bg-black ${Location.pathname === link.PageName ? 'bg-black ' : ''}`}>
+        <Link to={link.Path} key={index} className={`px-4 py-5 sm:px-8 flex items-center gap-x-5 hover:bg-black ${Location.pathname === link.Path ? 'bg-black ' : ''}`}>
         <span className='text-[1.2rem]'>{link.icon}</span>
         <span className='font-medium'>{link.PageName}</span>
         </Link>
@@ -91,11 +91,12 @@ export default function NavBar() {
       </ul>
       </div>  
 
-
+   {/* Logo */}
       <div>
       <img src={Logo} alt="Tech Nova" className=' w-[7.5rem] md:w-[10rem] cursor-pointer'/>
       </div>
 
+   {/* NavBar */}
       <ul className='hidden xl:flex gap-x-14 text-white'>
         {Links.map((link,index) => (
         <li key={index}><Link to={link.Path} className={`font-semibold hover:text-blue-400 ${Location.pathname === link.Path ?'text-blue-400':''}`}>{link.PageName}</Link></li>
